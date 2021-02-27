469abe9cd41d0c2c85a01f14a33893cb87577bad
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-02-27 22:54:17 +0530

    initial commit

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import AboutUsSection from "../components/AboutUsSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
+
+const AboutUs = () => {
+	return (
+		<>
+			<AboutUsSection />
+			<ServicesSection />
+			<FaqSection />
+		</>
+	);
+};
+
+export default AboutUs;
