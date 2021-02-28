597bd788817027204cbeb64584ed1cde85e01fb3
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-02-28 22:06:27 +0530

    style - ServicesSection

--- src/components/AboutUsSection.jsx
+++ src/components/AboutUsSection.jsx
@@ -3,6 +3,7 @@ import home1 from "../img/home1.png";
 
 //styled
 import styled from "styled-components";
+import { About, Description, Image, Hide } from "../style";
 
 const AboutUsSection = () => {
 	return (
@@ -34,37 +35,5 @@ const AboutUsSection = () => {
 		</About>
 	);
 };
-
-//& now this becomes a component that is styled.
-const About = styled.div`
-	min-height: 90vh;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 5rem 10rem;
-	color: white;
-`;
-
-const Description = styled.div`
-	flex: 1;
-	padding-right: 5rem;
-	h2 {
-		font-weight: lighter;
-	}
-`;
-
-const Image = styled.div`
-	flex: 1;
-	overflow: hidden;
-	img {
-		width: 100%;
-		height: 80vh;
-		object-fit: cover;
-	}
-`;
-
-const Hide = styled.div`
-	overflow: hidden;
-`;
 
 export default AboutUsSection;
