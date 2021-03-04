8ad90de3b7d7d2dd58711a3ab4346b9e22ef8d66
Biswanath Tewari <40662234+biZan7X@users.noreply.github.com>
2021-03-04 22:41:24 +0530

    feat - fixed services animation

--- src/animation.js
+++ src/animation.js
@@ -86,9 +86,10 @@ export const sliderHolder = {
 };
 
 export const scrollReveal = {
-	hidden: { opacity: 0, transition: { duration: 0.5 } },
+	hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
 	show: {
 		opacity: 1,
+		scale: 1,
 		transition: {
 			duration: 0.5,
 			ease: "easeOut",
